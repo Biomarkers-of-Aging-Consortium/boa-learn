bde0ede0790c4161db60b3f8e245bf225e2f5261
Seth Paulson <seth@tpfs.io>
2023-05-23 10:50:55 -0700

    Normalized loading of biomarker data

---
+++ boa-learn/load.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import unittest
+
+from model import load_columns
+
+def load_fhs():
+    """Loads data from the Framingham Heart Study"""
+    public_link='https://raw.githubusercontent.com/singator/bdah/master/data/frmgham2.csv'
+    df = pd.read_csv(public_link,index_col=0,usecols=['RANDID','PERIOD','AGE','SEX','DEATH','TIMEDTH','GLUCOSE'])
+    df=df[df['PERIOD']==1].drop('PERIOD',axis=1)
+    df['TIMEDTH']=df['TIMEDTH']/30.437 # days to months
+    df.index.name = 'id'
+    df=df.rename({
+        'AGE': 'age',
+        'SEX': 'sex',
+        'GLUCOSE': 'glucose',
+        'DEATH': 'is_dead',
+        'TIMEDTH': 'months_until_death'
+    },axis=1)
+    return df
+
+def load_nhanes(year):
+    """Loads data from the National Health and Nutrition Examination Survey"""
+    cbc_sub=['LBXRDW','LBXWBCSI','LBXLYPCT','LBDLYMNO','LBXRBCSI','LBXHGB','LBXPLTSI']
+
+    dem_path=f'https://wwwn.cdc.gov/Nchs/Nhanes/{year-1}-{year}/DEMO_F.XPT'
+    gluc_path=f'https://wwwn.cdc.gov/Nchs/Nhanes/{year-1}-{year}/GLU_F.XPT'
+    cbc_path=f'https://wwwn.cdc.gov/Nchs/Nhanes/{year-1}-{year}/CBC_F.XPT'
+    mortality_path=f'https://ftp.cdc.gov/pub/Health_Statistics/NCHS/datalinkage/linked_mortality/NHANES_{year-1}_{year}_MORT_2019_PUBLIC.dat'
+    dem=pd.read_sas(dem_path,index='SEQN')[['RIAGENDR','RIDAGEYR']]
+    dem.index=dem.index.astype(int)
+    gluc=pd.read_sas(gluc_path,index='SEQN')['LBDGLUSI']
+    gluc.index=gluc.index.astype(int)
+    cbc=pd.read_sas(cbc_path,index='SEQN')[cbc_sub]
+    cbc.index=cbc.index.astype(int)
+    mort=pd.read_fwf(mortality_path,index_col=0,header=None,widths=[14,1,1,3,1,1,1,4,8,8,3,3])
+    mort.index=mort.index.rename('SEQN')
+    dead=mort[mort[1]==1][[2,10]].astype(int)
+    dead.columns=['MORTSTAT','PERMTH_EXM']
+    df=pd.concat([dem,gluc,cbc,dead],axis=1).dropna()
+    df.index.name = 'id'
+    df=df.rename({
+        'RIDAGEYR': 'age',
+        'RIAGENDR': 'sex',
+        'LBDGLUSI': 'glucose',
+        'MORTSTAT': 'is_dead',
+        'PERMTH_EXM': 'months_until_death'
+    },axis=1)
+    df=df.rename({'LB2RDW':'LBXRDW','LB2WBCSI':'LBXWBCSI'},axis=1)
+    return df
+
+###Tests
+import pytest
+
+def test_fhs_columns():
+    df = load_fhs()
+    verify_expected_columns(df)
+
+def test_nhanes_columns():
+    df = load_nhanes(2010)
+    verify_expected_columns(df)
+
+
+def verify_expected_columns(df):
+    actual_columns = set(df.columns.to_list())
+    missing_columns = set(load_columns) - actual_columns
+    extra_columns = actual_columns - set(load_columns)
+    assert len(missing_columns) == 0, f"Missing expected columns: {missing_columns} \n Found extra columns: {extra_columns}"
+
+# Run the test
+if __name__ == '__main__':
+    pytest.main([__file__])
